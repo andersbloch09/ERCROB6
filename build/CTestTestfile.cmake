92ad6ba4aae34c52226beb8a34ae685666ff6db7
andersbloch09 <andersbloch09@gmail.com>
2024-03-22 15:53:57 +0100

    Initial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/bloch/ws_ur/src
+# Build directory: /home/bloch/ws_ur/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ur3e_config_made")
+subdirs("newur")
+subdirs("ur_control")
---
+++ build/ur_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bloch/ws_ur/src/ur_control
+# Build directory: /home/bloch/ws_ur/build/ur_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
